fcafd51d6156f834d36c7f381356a9c683611141
Mikulas Machovec <mikimachovec@gmail.com>
2025-01-07 14:59:58 +0000

    feat: argument acess

--- element.sh
+++ element.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
 PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
+
+if [[ -z $1 ]]
+then
+  echo "Please provide an element as an argument." 
+else 
+  echo $1
+fi
