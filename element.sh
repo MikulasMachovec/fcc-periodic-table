356824fa18261a1bd8e76f38d8e5d79b2a3f55c8
Mikulas Machovec <mikimachovec@gmail.com>
2025-01-07 16:57:06 +0000

    feat: first protoype

--- element.sh
+++ element.sh
@@ -14,14 +14,24 @@ else
   # $1 is a text
   ATOMIC_NUMBER=$($PSQL "SELECT atomic_number FROM elements INNER JOIN properties USING(atomic_number) WHERE name='$1' OR symbol='$1' " )
   fi
-  echo $ATOMIC_NUMBER
-  # get name
-  #NAME=$($PSQL "SELECT FROM WHERE atomic")
-  # get symbol
-  # get boiling_point
-  # get melting_point
-  # get type
+  if [[ -z $ATOMIC_NUMBER ]]
+  then
+    echo "I could not find that element in the database."
+  else
+    # get name
+    NAME=$($PSQL "SELECT name FROM elements WHERE atomic_number=$ATOMIC_NUMBER")
+    # get symbol
+    SYMBOL=$($PSQL "SELECT symbol FROM elements WHERE atomic_number=$ATOMIC_NUMBER")
+    # get boiling_point
+    BOILING=$($PSQL "SELECT boiling_point_celsius FROM properties WHERE atomic_number=$ATOMIC_NUMBER")
+    # get melting_point
+    MELTING=$($PSQL "SELECT melting_point_celsius FROM properties WHERE atomic_number=$ATOMIC_NUMBER")
+    # get type
+    TYPE_ID=$($PSQL "SELECT type_id FROM properties WHERE atomic_number=$ATOMIC_NUMBER")
+    TYPE=$($PSQL "SELECT type FROM types WHERE type_id=$TYPE_ID" )
+
+    echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING celsius and a boiling point of $BOILING celsius."
+  fi
 
 
 fi
-  
