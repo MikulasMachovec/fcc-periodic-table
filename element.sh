28268c3348f02ef582180983a6fdd5a28e7842fd
Mikulas Machovec <mikimachovec@gmail.com>
2025-01-07 16:32:11 +0000

    feat: get data by correct argument

--- element.sh
+++ element.sh
@@ -5,5 +5,19 @@ if [[ -z $1 ]]
 then
   echo "Please provide an element as an argument." 
 else 
-  echo $1
+  #check if argument is a number
+  if [[ "$1" =~ ^[0-9]+$ ]]
+  then
+  # $1 is a number
+  ATOMIC_NUMBER=$($PSQL "SELECT atomic_number FROM elements WHERE atomic_number=$1" )
+  else
+  # $1 is a text
+  ATOMIC_NUMBER=$($PSQL "SELECT atomic_number FROM properties WHERE name='$1' OR symbol='$1' " )
+  fi
+  # find name
+  # find symbol
+  # find boiling
+
+
 fi
+  
